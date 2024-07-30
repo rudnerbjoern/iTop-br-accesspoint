0d57509ee6589fd8e5f1e7d9458d3392a17ea522
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-07-30 12:53:00 +0200

    add dictionary

--- br-accesspoint/en.dict.br-accesspoint.php
+++ br-accesspoint/en.dict.br-accesspoint.php
@@ -8,7 +8,13 @@
  * Localized data
  */
 
-
 Dict::Add('EN US', 'English', 'English', array(
-    '' => '',
+    'Class:AccessPoint' => 'Access Point',
+    'Class:AccessPoint+' => 'Wireless Access Point',
+    'Class:AccessPoint/Attribute:policy_tag' => 'Policy Tag',
+    'Class:AccessPoint/Attribute:policy_tag+' => 'Policy Tag',
+    'Class:AccessPoint/Attribute:site_tag' => 'Site Tag',
+    'Class:AccessPoint/Attribute:site_tag+' => 'Site Tag',
+    'Class:AccessPoint/Attribute:rf_tag' => 'RF Tag',
+    'Class:AccessPoint/Attribute:rf_tag+' => 'RF Tag',
 ));
