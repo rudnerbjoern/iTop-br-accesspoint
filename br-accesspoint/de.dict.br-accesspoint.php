a3fb68fa1e207bae61ffa4dcf473bf4d8a174311
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-07-30 12:27:18 +0200

    initial commit

---
+++ br-accesspoint/de.dict.br-accesspoint.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-06-11
+ *
+ * Localized data
+ */
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+    '' => '',
+));
---
+++ br-accesspoint/en.dict.br-accesspoint.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2024 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2024-07-30
+ *
+ * Localized data
+ */
+
+
+Dict::Add('EN US', 'English', 'English', array(
+    '' => '',
+));
