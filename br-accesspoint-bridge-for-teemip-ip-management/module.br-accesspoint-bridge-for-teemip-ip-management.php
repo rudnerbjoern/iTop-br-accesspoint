1acdbb9bf520dabed19e1b9d1a33f4678b6d199a
Bjoern Rudner <rudnerbjoern@hotmail.com>
2024-09-04 09:26:33 +0200

    reenable model file for compatibility with iTop 2.7 installer

--- br-accesspoint-bridge-for-teemip-ip-management/module.br-accesspoint-bridge-for-teemip-ip-management.php
+++ br-accesspoint-bridge-for-teemip-ip-management/module.br-accesspoint-bridge-for-teemip-ip-management.php
@@ -3,14 +3,14 @@
 /**
  * @copyright   Copyright (C) 2024 Björn Rudner
  * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
- * @version     2024-08-30
+ * @version     2024-09-04
  *
  * iTop module definition file
  */
 
 SetupWebPage::AddModule(
     __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-    'br-accesspoint-bridge-for-teemip-ip-management/0.2.0',
+    'br-accesspoint-bridge-for-teemip-ip-management/0.2.1',
     array(
         // Identification
         'label' => 'Bridge - Access Point + TeemIP IPAM',
@@ -18,15 +18,17 @@ SetupWebPage::AddModule(
 
         // Setup
         'dependencies' => array(
-            'br-accesspoint/0.2.0||teemip-ip-mgmt/3.0.0',
-            'br-accesspoint/0.2.0',
+            'br-accesspoint/0.2.1||teemip-ip-mgmt/3.0.0',
+            'br-accesspoint/0.2.1',
         ),
         'mandatory' => false,
         'visible' => true,
         'auto_select' => 'SetupInfo::ModuleIsSelected("br-accesspoint") && SetupInfo::ModuleIsSelected("teemip-ip-mgmt")',
 
         // Components
-        'datamodel' => array(),
+        'datamodel' => array(
+            'model.br-accesspoint-bridge-for-teemip-ip-management.php',
+        ),
         'webservice' => array(),
         'dictionary' => array(),
         'data.struct' => array(),
